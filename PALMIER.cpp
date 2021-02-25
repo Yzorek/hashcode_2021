5881c2db150f2355b6fb713b1d9e15c117179b46
Charles <charles.affouard@epitech.eu>
2021-02-25 21:24:15 +0100

    [+] add something ahahahahahahahahhahahhahahahhah

---
+++ PALMIER.cpp
@@ -0,0 +1,37 @@
+#include "Parser.hpp"
+#include "Ilian.hpp"
+#include "PALMIER.hpp"
+
+
+void intersection(int int1, std::string namefrérot)
+{
+    this->name = int1;
+    this->street = (namefrérot + " ");
+}
+
+void ~intersection()
+{
+    //DESTROY ANYTHING
+}
+
+void assemblage()
+{
+    for (int x = 0 ; x < "LE NB D'INTERSECTION" ; x++) {
+        int first_intersection = Allstreet[x]._intersection1;
+        int second_intersection = Allstreet[x]._intersection2;
+        if (intersection.name = first_intersection1)
+            intersection.street = (Allstreet[x]._name + " ");    
+        else if (intersection.name = first_intersection2)
+            intersection.street = (Allstreet[x]._name + " ");
+        else
+            intersection.intersection(Allstreet[x].__intersection1, Allstreet[x]._name + " ");
+            intersection.intersection(Allstreet[x].__intersection2, Allstreet[x]._name + " ");
+    }
+
+    for (int x = 0 ; x < "LE NB DE INTERSECTION" ; x++) {
+        /*
+            Faut marquer le nb d'intersection et le nb de rue 
+            ensuite mettre toute les rues les rues entre intersection puis 5 sec
+        */
+    }
+}
---
+++ PALMIER.hpp
@@ -0,0 +1,18 @@
+#ifndef PALMIER_HPP_
+#define PALMIER_HPP_
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class intersection {
+    public:
+        intersection();
+        ~intersection();
+    protected:
+        int name;
+        std::string street;
+    private:
+};
+
+#endif /* !PARSER_HPP_ */
