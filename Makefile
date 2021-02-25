cf6a97461112f41917b28a3808a12d1ebd0369b4
Lenglos Constantin <constantin.lenglos@epitech.eu>
2021-02-25 19:24:17 +0100

    [test]

---
+++ Makefile
@@ -0,0 +1,27 @@
+##
+## EPITECH PROJECT, 2019
+## Makefile rush 2
+## File description:
+## Makefile
+##
+
+SRC		=		main.cpp	\
+
+OBJ		=		$(SRC:.cpp=.o)
+
+NAME		=		my_traffic
+
+all:		$(NAME)
+
+CFLAGS	= 	-I./ -Wall -Wextra -Werror
+
+$(NAME):	$(OBJ)
+	g++ -o $(NAME) $(OBJ) $(CFLAGS)
+
+clean:
+	rm	-f 	$(OBJ)
+
+fclean: clean
+	rm 	-f 	$(NAME)
+
+re:	fclean	all
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void openFile(char **av)
+{
+    int j = 0;
+    char test;
+
+    while (av[++j] != NULL) {
+        std::ifstream my_file(av[j], std::ios::in);
+        if (!my_file) {
+        std::cerr << "my_traffic: " << av[j] <<": No such file or directory"<< std::endl;
+    } else
+    while (my_file.get(test))
+        std::cout << test;
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 1) {
+        std::cerr << "my_traffic: Usage: ./my_cat file [...]" << std::endl;
+        return 84;
+    }
+    if (ac >= 2)
+        openFile(av);
+
+    return 0;
+}
